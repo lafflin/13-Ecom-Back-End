0f6d821fb07188ff34a75515fa3a21a9b20897df
Max McLaughlin <maxwelljmack@gmail.com>
2022-09-22 21:07:20 -0700

    got the models made, seeding now works. associations next.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false,
+		},
+		product_id: {
+			type: DataTypes.INTEGER,
+		},
+		tag_id: {
+			type: DataTypes.INTEGER,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "product_tag",
+	}
 );
 
 module.exports = ProductTag;
